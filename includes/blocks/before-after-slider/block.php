e0e16f2a1d6d7cf37fc69d2a8a93909de92a1942
himanshudhakan <himanshudhakan9@gmail.com>
2024-02-29 16:36:55 +0530

    base plugin

---
+++ includes/blocks/before-after-slider/block.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Before after slider block core file.
+ *
+ * @link       https://elicus.com
+ * @since      1.0.0
+ *
+ * @package    WPMozo_Addons_For_Gutenberg
+ * @subpackage WPMozo_Addons_For_Gutenberg/includes/blocks/before-after-slider
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
---
+++ includes/class-wpmozo-addons-for-gutenberg-init.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Define the hooks for WP initialization.
+ *
+ * @link       https://elicus.com
+ * @since      1.0.0
+ *
+ * @package    WPMozo_Addons_For_Gutenberg
+ * @subpackage WPMozo_Addons_For_Gutenberg/includes
+ */
+
+/**
+ * This class responsible for defining all actions for WP initialization of the plugin.
+ *
+ * @since      1.0.0
+ * @package    WPMozo_Addons_For_Gutenberg
+ * @subpackage WPMozo_Addons_For_Gutenberg/includes
+ * @author     Elicus <hello@elicus.com>
+ */
+class WPMozo_Addons_Gutenberg_Init {
+
+	/**
+	 * Add all hooks.
+	 *
+	 * @since 1.0.0
+	 * @param array $loader The instance of loader class.
+	 * @param array $instance The instance of this class.
+	 */
+	public function add_hooks( $loader, $instance ) {
+
+
+	}
+
+}
