7d67df4d7db5d695f91264e43c8b7cbead676c8b
himanshudhakan <himanshudhakan9@gmail.com>
2024-03-26 13:22:14 +0530

    Fix - save structure.

--- src/blocks/fancy-heading/js/save.js
+++ src/blocks/fancy-heading/js/save.js
@@ -2,19 +2,28 @@
 import Style from "./style";
 
 import { useBlockProps } from "@wordpress/block-editor";
+import { createElement } from "@wordpress/element";
 
 const Save = ({ attributes }) => {
 
     const clientId = attributes.clientId;
 
+    const allText = (
+        <>
+            <span className="wpmozo-adfgu-pre-text">{ attributes.preHeading }</span>
+            <span className="wpmozo-adfgu-main-text">{ attributes.mainHeading }</span>
+            <span className="wpmozo-adfgu-post-text">{ attributes.postHeading }</span>
+        </>
+    );
+
+    const heading = createElement( attributes.headingLavel, {
+        className: 'wpmozo-adfgu-fancy-heading-inner'
+    }, allText );
+
     return (
-        <div {...useBlockProps.save({ className: 'wpmozo-adfgu-fancy-headign-main' })} id={`block-${clientId}`}>
+        <div {...useBlockProps.save({ className: 'wpmozo-adfgu-fancy-heading-wrap' })} id={`block-${clientId}`}>
             <Style {...attributes} />
-            <div className="wpmozo-adfgu-fancy-heading-inner">
-                <span className="wpmozo-adfgu-pre-text">{ attributes.preHeading }</span>
-                <span className="wpmozo-adfgu-main-text">{ attributes.mainHeading }</span>
-                <span className="wpmozo-adfgu-post-text">{ attributes.postHeading }</span>
-            </div>
+            {heading}
         </div>
     );
 };
