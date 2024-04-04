887f9a19f0410cd3fa8a8472ad0acf8ed3679128
himanshudhakan <himanshudhakan9@gmail.com>
2024-04-04 12:20:28 +0530

    Fix - overlay issue and changed style for editor.

--- src/blocks/fancy-text/js/style.js
+++ src/blocks/fancy-text/js/style.js
@@ -1,7 +1,8 @@
 
 const Style = (attributes) => {
 
-	const clientId = attributes.clientId,
+	const wpmozoCoreFun = window.wpmozo,
+	clientId = attributes.clientId,
     parent = '#block-'+clientId,
     toConvertStyles = [
     	'text',
@@ -9,7 +10,7 @@ const Style = (attributes) => {
     ];
 
     let css = '', 
-    convertedStyle = window.wpmozo.convetInlineStyleStr( toConvertStyles, attributes );
+    convertedStyle = wpmozoCoreFun.convetInlineStyleStr( toConvertStyles, attributes );
 
     let allInline = [
 		{
@@ -25,6 +26,7 @@ const Style = (attributes) => {
     ];
 
     if ( 'gradient' === attributes.textStyle ) {
+
     	allInline.push({
     		selector: '.wpmozo-adfgu-fancy-text-inner',
     		style: {
@@ -32,7 +34,9 @@ const Style = (attributes) => {
 			    'background-image': attributes.fancyTextBackground,
     		}
     	})
+
     }else{
+
     	allInline.push({
     		selector: '.wpmozo-adfgu-fancy-text-inner',
     		style: {
@@ -42,50 +46,51 @@ const Style = (attributes) => {
 			    'background-repeat': attributes.textBgRepeat,
     		}
     	})
+
+    	if ( 'none' !== attributes.textBgOverlay ) {
+			allInline.push({
+	    		selector: '.wpmozo-adfgu-fancy-text-inner:before',
+	    		style: {
+	    			'content': '""',
+	    			'position': 'absolute',
+				    'top': '0',
+				    'right': '0',
+				    'bottom': '0',
+				    'left': '0',
+				    'z-index': '-2',
+				    'background-image': 'inherit',
+				    'background-size': attributes.textBgSize,
+				    'background-position': attributes.textBgPosition,
+				    'background-repeat': attributes.textBgRepeat,
+	    		},
+	    	})
+	    	allInline.push({
+	    		selector: '.wpmozo-adfgu-fancy-text-inner:after',
+	    		style: {
+	    			'content': '""',
+	    			'position': 'absolute',
+				    'top': '0',
+				    'right': '0',
+				    'bottom': '0',
+				    'left': '0',
+				    'z-index': '-1',
+				    ...('color' === attributes.textBgOverlay && {'background-color': attributes.textBgOverlayColor}),
+				    ...('gradient' === attributes.textBgOverlay && {'background': attributes.textBgOverlayColor})
+	    		},
+	    	})
+	    	css += `
+		    	${parent} {
+		    		z-index: 0;
+		    		position: relative;
+		    	}
+		    `;
+	    }
+
     }
 
-    if ( 'none' !== attributes.textBgOverlay ) {
-		allInline.push({
-    		selector: '.wpmozo-adfgu-fancy-text-inner:before',
-    		style: {
-    			'content': '""',
-    			'position': 'absolute',
-			    'top': '0',
-			    'right': '0',
-			    'bottom': '0',
-			    'left': '0',
-			    'z-index': '-2',
-			    'background-image': 'inherit',
-			    'background-size': attributes.textBgSize,
-			    'background-position': attributes.textBgPosition,
-			    'background-repeat': attributes.textBgRepeat,
-    		},
-    	})
-    	allInline.push({
-    		selector: '.wpmozo-adfgu-fancy-text-inner:after',
-    		style: {
-    			'content': '""',
-    			'position': 'absolute',
-			    'top': '0',
-			    'right': '0',
-			    'bottom': '0',
-			    'left': '0',
-			    'z-index': '-1',
-			    ...('color' === attributes.textBgOverlay && {'background-color': attributes.textBgOverlayColor}),
-			    ...('gradient' === attributes.textBgOverlay && {'background': attributes.textBgOverlayColor})
-    		},
-    	})
-    	css += `
-	    	${parent} {
-	    		z-index: 0;
-	    		position: relative;
-	    	}
-	    `;
-    }
-
-    let generateStyle = window.wpmozo.wpmozo_generate_style(allInline);
+    let generateStyle = wpmozoCoreFun.wpmozo_generate_style(allInline);
     
-    if ( ! window.wpmozo.wpmozo_is_empty( generateStyle ) ) {
+    if ( ! wpmozoCoreFun.wpmozo_is_empty( generateStyle ) ) {
 	    css += `
 	    	${parent} {
 	    		${generateStyle}
@@ -93,7 +98,7 @@ const Style = (attributes) => {
 	    `;
 	}
 
-	if ( ! window.wpmozo.wpmozo_is_empty( convertedStyle.textBgDimensions ) ) {
+	if ( ! wpmozoCoreFun.wpmozo_is_empty( convertedStyle.textBgDimensions ) && 'none' !== attributes.textBgOverlay ) {
 		css += `
 	    	${parent} {
 	    		${convertedStyle.textBgDimensions}
@@ -103,7 +108,7 @@ const Style = (attributes) => {
 
 	return (
 		<>
-			{ ! window.wpmozo.wpmozo_is_empty( css ) &&
+			{ ! wpmozoCoreFun.wpmozo_is_empty( css ) &&
 				<style>{css}</style>
 			}
 		</>
